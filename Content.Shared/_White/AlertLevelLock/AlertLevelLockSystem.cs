59b2ff5007922e5a4de26d803a26956d5fd3ae8a
nukkuminen <90336027+oldschoolotaku@users.noreply.github.com>
2025-11-02 17:01:14 +0300

    Ported alert level code lock

---
+++ Content.Shared/_White/AlertLevelLock/AlertLevelLockSystem.cs
@@ -0,0 +1,39 @@
+﻿using Content.Shared._White.AlertLevelLock.Components;
+using Content.Shared.Lock;
+using Content.Shared.Popups;
+
+namespace Content.Shared._White.AlertLevelLock;
+
+public sealed class SharedStationAlertLevelLockSystem : EntitySystem
+{
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StationAlertLevelLockComponent, LockToggleAttemptEvent>(OnTryAccess);
+    }
+
+    private void OnTryAccess(Entity<StationAlertLevelLockComponent> ent, ref LockToggleAttemptEvent args)
+    {
+        if (!TryComp<LockComponent>(ent.Owner, out var lockComponent))
+            return;
+
+        if (!ent.Comp.Enabled || !ent.Comp.Locked)
+            return;
+
+        // If the user is trying to lock the object (not unlock), allow it.
+        var isLocking = !lockComponent.Locked;
+        if (isLocking)
+            return;
+
+        _popup.PopupClient(
+            Loc.GetString("access-failed-wrong-station-alert-level"),
+            ent.Owner,
+            args.User
+        );
+
+        args.Cancelled = true;
+    }
+}
---
+++ Content.Shared/_White/AlertLevelLock/Components/AlertLevelLockComponent.cs
@@ -0,0 +1,32 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._White.AlertLevelLock.Components;
+
+/// <summary>
+/// Component that locks entities based on the current station alert level.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class StationAlertLevelLockComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public bool Enabled = true;
+
+    [DataField, AutoNetworkedField]
+    public bool Locked = true;
+
+    /// <summary>
+    /// Set of alert levels that will cause this entity to be locked.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public HashSet<string> LockedAlertLevels = [];
+
+    /// <summary>
+    /// Tracks the previous alert level. Needed for system to work fine with Corvax SOP
+    /// </summary>
+    /// TODO: Make it able to save previous code and check it properly
+    [DataField, AutoNetworkedField]
+    public string PreviousAlertLevel = "green";
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? StationId;
+}
